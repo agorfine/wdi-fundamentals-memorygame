801c72d5821abbb8663472cc23b5f5b36dc36423
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro.local>
2018-09-01 18:33:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,53 @@
 
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: 'queen',
+		suit: 'diamond',
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: "images/king-of-hearts.png"
+
+	},
+	{
+		rank: 'king',
+		suit: 'spades',
+		cardImage: "images/king-of-diamonds.png"
+	}
+];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped " + cardTwo);
-
-if(cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1]){
+var checkForMatch = function() {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");
 	}
-}
+};
+
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId].rank)
+	if (cardsInPlay.leng === 2) {
+		checkForMatch()
+	}
+	console.log("User flipped " + cards[cardId].rank)
+	console.log(cards[cardId].cardImage)
+	console.log(cards[cardId].suit)
+};
+
+flipCard(0);
+flipCard(1);
+
+
+
+
+
 
