c32dd7b36f44044d5c38a62b55ec3bedad176ab0
Amanda Gorfine <amandagorfine@Amandas-MacBook-Pro.local>
2018-09-02 11:55:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -26,27 +26,38 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if(cardsInPlay[0] === cardsInPlay[1]) {
+	if(cardsInPlay[0] === cardsInPlay [1]) {
 		alert("You found a match");
 	} else {
 		alert("Sorry, try again");
 	}
 };
 
-var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId].rank)
-	if (cardsInPlay.leng === 2) {
-		checkForMatch()
-	}
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
+
 	console.log("User flipped " + cards[cardId].rank)
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage)
 	console.log(cards[cardId].suit)
+
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(1);
+var createBoard = function () {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
 
-
+createBoard ();
 
 
 
